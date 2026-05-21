34ec95532ffaa225ddfa378dd7f29c27ede54906
khartahk <1172389-khartahk@users.noreply.gitlab.com>
2026-05-21 21:39:59 +0200

    fix(desktop): update launcher done message to reflect kgx close behaviour

--- modules/desktop/rebuild-launcher.nix
+++ modules/desktop/rebuild-launcher.nix
@@ -26,7 +26,7 @@ let
       Comment=${description}
       Comment[sl]=${descriptionSl}
       Icon=${icon}
-      Exec=kgx -- bash -c "pkexec sh -c 'nix flake update /etc/nixos && nixos-rebuild ${operation} --flake ${flakeArg}'; echo; echo '--- Done. Press Enter to close. ---'; read"
+      Exec=kgx -- bash -c "pkexec sh -c 'nix flake update /etc/nixos && nixos-rebuild ${operation} --flake ${flakeArg}'; echo; echo '--- Done. Close this window to continue. ---'"
       Terminal=false
       Categories=System;Settings;
       Keywords=update;rebuild;nixos;system;
