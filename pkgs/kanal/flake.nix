9d0b785ee70d8ddc0860768fc413cf7faa4ec192
khartahk <1172389-khartahk@users.noreply.gitlab.com>
2026-05-28 09:20:40 +0200

    bump: version → v0.6.3

--- pkgs/kanal/flake.nix
+++ pkgs/kanal/flake.nix
@@ -25,7 +25,7 @@
       packages.${system} = {
         kanal = pkgs.python3Packages.buildPythonApplication {
           pname   = "kanal";
-          version = "0.6.2";
+          version = "0.6.3";
           src     = ./.;
           format  = "pyproject";
           nativeBuildInputs = with pkgs; [
